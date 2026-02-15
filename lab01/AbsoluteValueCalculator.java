7482f2304b4dc57562dacf9c804c556b9c8302c0
Hamad <mh.atgsystems@gmail.com>
2026-02-16 03:40:16 +0500

    update lab 01

--- lab01/AbsoluteValueCalculator.java
+++ lab01/AbsoluteValueCalculator.java
@@ -1,20 +1,16 @@
 import java.util.Scanner;
 public class AbsoluteValueCalculator {
     public static void main(String[] args) {
-        // Scanner for user input
+        // Scanner for user input 
         Scanner scanner = new Scanner(System.in);
-
         // Ask user for input
-        System.out.print("Enter a number: ");
+        System.out.print("Please Enter a number: ");
         int number = scanner.nextInt();
-
         // Calculate absolute value
         int absoluteValue = Math.abs(number);
-
         // Print absolute value
-        System.out.println("Absolute value: " + absoluteValue);
-
-        // Close scanner
+        System.out.println("The Absolute value: " + absoluteValue);
+        // Close scanner 
         scanner.close();
     }
 }
